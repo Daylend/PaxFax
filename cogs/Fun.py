cfbd22267c3032eba9ca366ed267262930235f06
Daylend <Daylend@users.noreply.github.com>
2020-09-21 03:17:01 -0600

    Added joke command

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import aiohttp
+import random
 
 class Fun(commands.Cog):
 
@@ -29,6 +31,72 @@ class Fun(commands.Cog):
     async def _suggest(self, ctx, *, message):
         await self.bot.add_suggestion(message)
         await ctx.send("Your suggestion has been added to the suggestion box! Thank you")
+
+    @commands.command()
+    async def joke(self, ctx):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://official-joke-api.appspot.com/jokes/general/random') as r:
+                res = await r.json()
+                embed = discord.Embed(
+                    color=self.bot.embed_color,
+                    title="→ Random Joke!",
+                    description=f"• Question: {res[0]['setup']}"
+                                f"\n• Joke: {res[0]['punchline']}"
+                )
+                await ctx.send(embed=embed)
+
+    @commands.command()
+    async def coinflip(self, ctx):
+        choices = ("Heads!", "Tails!")
+        coin = random.choice(choices)
+        embed = discord.Embed(
+            color=self.bot.embed_color,
+            title="→ Coinflip Command",
+            description=f"• {coin}"
+        )
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name="8ball")
+    async def _8ball(self, ctx, *, question, ):
+        responses = ["It is certain.",
+                     "It is decidedly so.",
+                     "Without a doubt.",
+                     "Yes - definitely.",
+                     "You may rely on it.",
+                     "As I see it, yes.",
+                     "Most likely.",
+                     "Outlook good.",
+                     "Yes.",
+                     "Signs point to yes.",
+                     "Reply hazy, try again.",
+                     "Ask again later.",
+                     "Better not tell you now.",
+                     "Cannot predict now.",
+                     "Concentrate and ask again.",
+                     "Don't count on it.",
+                     "My reply is no.",
+                     "My sources say no.",
+                     "Outlook not so good.",
+                     "Very doubtful."]
+        embed = discord.Embed(
+            color=self.bot.embed_color,
+            title="→ 8Ball Command"
+        )
+        embed.add_field(name="• Question :grey_question: ", inline=False, value=f"{question}")
+        embed.add_field(name="• Answer :8ball: ", inline=False, value=f"{random.choice(responses)}")
+
+        await ctx.send(embed=embed)
+
+    @_8ball.error
+    async def _8ball_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                color=self.bot.embed_color
+            )
+            embed.add_field(name="→ Invalid Argument!",
+                            value="• Please put in a valid option! Example: `l!8ball <question>`")
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
